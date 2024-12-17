ce87b9833c25b1587c6cc404ea7d568ba61d3f8a
davidwinalda <davidwinalda94@gmail.com>
2024-12-17 17:43:30 +0700

    Initial commit - published button component demo

---
+++ app/controllers/components/button/buttons_controller.rb
@@ -0,0 +1,37 @@
+module Components
+  module Button
+    class ButtonsController < ApplicationController
+      def index
+        render "components/button/index"
+      end
+
+      def variant
+        render "components/button/variant"
+      end
+
+      def icon
+        render "components/button/icon"
+      end
+
+      def size
+        render "components/button/size"
+      end
+
+      def radius
+        render "components/button/radius"
+      end
+
+      def link
+        render "components/button/link"
+      end
+
+      def loading
+        render "components/button/loading"
+      end
+
+      def dropdown
+        render "components/button/dropdown"
+      end
+    end
+  end
+end
